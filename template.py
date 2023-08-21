f625ac7238b46f16be251e1670b55e9bad20c655
Mayur Suthar <mayur1601@yahoo.in>
2023-08-21 10:34:37 +0700

    Data Ingestion config updated

--- template.py
+++ template.py
@@ -10,7 +10,7 @@ project_name = 'textSummarizer'
 list_of_files = [
     '.github/workflow/.gitkeep',
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -37,12 +37,15 @@ for filepath in list_of_files:
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory {filedir} for the file {filename}")
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    if (not os.path.exists(filepath) or os.path.getsize(filename) == 0):
-        with open(filepath, "w") as f:
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
             pass
-            logging.info(f"Created empth file {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
+
+    
     else:
-        logging.info(f"File {filename} already exists")
+        logging.info(f"{filename} is already exists")

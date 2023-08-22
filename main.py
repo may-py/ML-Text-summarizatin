d507840090712bb6dc387229dbe6df1a073d679d
Mayur Suthar <mayur1601@yahoo.in>
2023-08-22 09:43:25 +0700

    Data Validation stage updated

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
-# logger.info("Welcome to our custom log")
+logger.info("Welcome to our custom log")
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -9,6 +10,18 @@ try:
     logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<<')
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:    
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<<')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<')
 except Exception as e:
     logger.exception(e)

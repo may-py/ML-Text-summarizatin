e5d20ac0b281a45d45d2ecdf9f4b035ecf56d4e1
Mayur Suthar <mayur1601@yahoo.in>
2023-08-23 13:24:57 +0700

    model training

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_training import ModelTrainerTrainingPipeline
+
+
+
 
 logger.info("Welcome to our custom log")
 
@@ -22,6 +27,30 @@ try:
     logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<<')
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data Transformation Stage'
+
+try:    
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<<')
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Model Training Stage'
+
+try:    
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<<')
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<')
 except Exception as e:
     logger.exception(e)
